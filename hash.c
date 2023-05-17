88e8180951f6a0842c1d3a5763c0de70a0aa09f5
egerin11 <111665202+egerin11@users.noreply.github.com>
2023-05-17 11:43:25 +0300

    Update hash.c

--- hash.c
+++ hash.c
@@ -7,7 +7,7 @@ unsigned int murMurHash(const char *key, unsigned int seed) {
     unsigned int h = seed ^ len;
     const unsigned char *data = (const unsigned char *) key;
     while (len >= 4) {
-        unsigned int k = *(unsigned int *)(void *)data;  // Явно приводим к void * сначала
+        unsigned int k = *(unsigned int *)data;  
         k *= m;
         k ^= k >> r;
         k *= m;
@@ -19,14 +19,11 @@ unsigned int murMurHash(const char *key, unsigned int seed) {
     switch (len) {
         case 3:
             h ^= data[2] << 16;
-            /* fall through */
-        case 2:
+              case 2:
             h ^= data[1] << 8;
-            /* fall through */
-        case 1:
+          case 1:
             h ^= data[0];
             h *= m;
-            /* fall through */
         default:
             break;
     }
